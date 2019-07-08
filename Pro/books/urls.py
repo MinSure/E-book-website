a289314c573f1b72167cd5341d032490263f27bd
MinSure <asd939117071@163.com>
2019-07-08 23:56:42 +0800

    first commit

---
+++ Pro/books/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from books import views
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('delete/<int:book_id>/',views.delete),
+    path('goupdate/<int:book_id>/',views.goupdate),
+    path('update/',views.update),
+    path('<int:book_id>',views.detail),
+    path('types/<int:type_id>',views.types),
+    path('download/<int:book_id>',views.filedownload),
+    path('paihang/',views.paihang),
+]
